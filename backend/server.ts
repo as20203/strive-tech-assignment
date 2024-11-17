bb5f6ef2512fe1849f8971cc31c868c94da40820
Jawad Zaheer <jzaheer.bese16seecs@seecs.edu.pk>
2024-11-17 19:14:50 +0200

    Added update to port number for avoiding conflicts.

--- backend/server.ts
+++ backend/server.ts
@@ -15,7 +15,7 @@ register({
 });
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(cors({
     origin: ['http://localhost', 'http://localhost:3000']
